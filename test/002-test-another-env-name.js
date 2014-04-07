72337d09a0f671fa7e6c05d25195d8118a8a9499
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2014-04-07 18:57:24 -0300

    Fix tests

--- test/002-test-another-env-name.js
+++ test/002-test-another-env-name.js
@@ -1,31 +1,34 @@
 var path = require('path');
-
-process.env.NODE_ENV = 'production.001';
-var env = require('../lib/index')(path.join(__dirname, '/.econf.js'), 'NODE_ENV');
-
 var assert = require("assert")
-describe('Production.001', function(){
+
+describe('Test Production.001 with custom env name', function(){
+	before(function(){
+		process.env = {};
+		process.env.NODE_ENV = 'production.001';
+		require('../lib/index')(path.join(__dirname, '/.econf.js'), 'NODE_ENV');
+	});
+
 	describe('NAME', function(){
 		it('should return "Production 001"', function(){
 			assert.equal('Production 001', process.env.NAME);
-		})
-	})
+		});
+	});
 
 	describe('AWS_SECRET', function(){
 		it('should return "aws-secret"', function(){
 			assert.equal('aws-secret', process.env.AWS_SECRET);
-		})
-	})
+		});
+	});
 
 	describe('AWS_KEY', function(){
 		it('should return "aws-key"', function(){
 			assert.equal('aws-key', process.env.AWS_KEY);
-		})
-	})
-	
+		});
+	});
+
 	describe('EMAIL_DELAY', function(){
 		it('should return 0', function(){
 			assert.equal(0, process.env.EMAIL_DELAY);
-		})
-	})
-})
+		});
+	});
+});
